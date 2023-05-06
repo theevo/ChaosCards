dba6833d5100c793c6ff4c1f79c1bca464e801c7
Theo Vora <theo.vora@gmail.com>
2023-05-06 13:01:47 -0500

    Initial Commit

---
+++ Chaos Cards/Chaos_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chaos_CardsApp.swift
+//  Chaos Cards
+//
+//  Created by Theo Vora on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Chaos_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chaos CardsUITests/Chaos_CardsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chaos_CardsUITestsLaunchTests.swift
+//  Chaos CardsUITests
+//
+//  Created by Theo Vora on 5/6/23.
+//
+
+import XCTest
+
+final class Chaos_CardsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
