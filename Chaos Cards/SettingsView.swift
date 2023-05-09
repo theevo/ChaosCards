b722b6fb9701f6bd82cc5642a2d96ba294a98a80
Theo Vora <theo.vora@gmail.com>
2023-05-09 12:35:08 -0500

    Add settings for Enable Notifications and Notification Frequency

--- Chaos Cards/SettingsView.swift
+++ Chaos Cards/SettingsView.swift
@@ -7,9 +7,33 @@
 
 import SwiftUI
 
+enum NotificationFrequency: String, CaseIterable {
+    case Seconds5 = "5 seconds"
+    case Seconds10 = "10 seconds"
+    case Minutes1 = "1 minute"
+    case Minutes3 = "3 minutes"
+    case Surprise = "Surprise me"
+}
+
 struct SettingsView: View {
+    @State private var isNotificationsOn = false
+    @State private var notificationFrequency: NotificationFrequency = .Minutes1
     var body: some View {
-        Text("Welcome to Settings, pilgrim")
+        Form {
+            Section(header: Text("Notifications")) {
+                Toggle(isOn: $isNotificationsOn) { Text("Notifications")
+                }
+                Picker(
+                    selection: $notificationFrequency,
+                    label: Text("Quiz me every")
+                ) {
+                    ForEach(NotificationFrequency.allCases, id: \.self) {
+                        Text($0.rawValue).tag($0)
+                    }
+                }
+            }
+            
+        }
     }
 }
 
