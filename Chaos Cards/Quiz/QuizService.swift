e0e61cc9f01b27873122d2f8500b4812681d8107
Theo Vora <theo.vora@gmail.com>
2023-05-12 10:13:04 -0500

    Enable quiz service to setup questions with option like number of wrong choices

--- Chaos Cards/Quiz/QuizService.swift
+++ Chaos Cards/Quiz/QuizService.swift
@@ -9,4 +9,15 @@ import Foundation
 
 struct QuizService {
     let quiz: Quiz
+    private(set) var remainingQuestions: [Question] = []
+}
+
+extension QuizService {
+    mutating func setupQuestions(numberOfWrongChoices: Int = 2) {
+        let question = Question(
+            prompt: "Capital of England",
+            correctChoice: Choice("London"),
+            wrongChoices: [Choice("Paris"), Choice("Milan"), Choice("Beijing"), Choice("Bangkok")])
+        remainingQuestions = [question]
+    }
 }
